3ce049414b73a1b9f47b310328a8d2448664e67d
solure-ace <av.lynne.w@gmail.com>
2024-09-20 14:15:03 -0600

    interactive scene sept20th

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,58 +3,45 @@
 // September 19th, 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// -Project resizes with the Window
+// 
+let borderSize = 60;
 
 
 function setup() {
-  createCanvas(255*2, 255*2);
+  createCanvas(windowWidth, windowHeight);
+  stuff();
+
 }
-
-
-// use html to show typed str =+ key w/ char limit, + button to clear it make text colour match the ball colour
-//somewhere to use nested loops?, maybe change something about the shape in every corner more ''while'' its in that corner,+size + size min, -size w/ size min, shapecircle, shape triangle
-
-let borderSize = 20
-let size = 100
 
 function draw() {
-  background(0);
-  
-  noStroke();
-  
-  
-  //white / middle
-   if (mouseX === width/2 && mouseY === height/2) {
-    fill(255, 255, 255);
-    circle(mouseX, mouseY, size);
-  }
-  
-   //blue / top left corner
-  else if(mouseX <= width/2 && mouseY <= height/2){
-    fill(120, 150, 250);
-    circle(mouseX, mouseY, size);
-  }
-  
-  //red / bottom right corner
-  else if (mouseX >= width/2 && mouseY >= height/2) {
-    fill(225, 100, 100);
-    circle(mouseX, mouseY, size);
-  }
-  
-  //purple / bottom left corner
-  else if (mouseX <= width/2 && mouseY >= height/2) {
-    fill(180, 80, 180);
-    circle(mouseX, mouseY, size);
-  }
-  
-  //green / top right corner
-  else if (mouseX >= width/2 && mouseY <= height/2){
-    fill(100, 210, 150);
-    circle(mouseX, mouseY, size);
-  }
 }
 
-//resizes it with a border
+function mousePressed() {
+  fill("black");
+  circle(mouseX, mouseY, 10);
+}
+
+function mouseDragged() {
+  fill("black");
+  circle(mouseX, mouseY, 10);
+}
+
+function smallCanvas() {
+  //smaller drawable area
+  noStroke();
+  fill(230)
+  rect(100, 100, 500)
+}
+
+function stuff() {
+  background(230, 200, 260);
+  smallCanvas();
+}
+
+
+//resizes the project with the window
 function windowResized() {
-  resizeCanvas(windowWidth-borderSize, windowHeight-borderSize);
+  resizeCanvas(windowWidth, windowHeight);
+  stuff();
 }
