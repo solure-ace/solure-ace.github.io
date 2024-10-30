55b4c7af558143792bb7c6432d679ff0f4c24026
solure-ace <av.lynne.w@gmail.com>
2024-10-30 14:15:10 -0600

    sudoku - number selection & possible nums
    randomly generating the sudoku random out of possible numbers - checkViableNumber(){}
    
    changed /grid/ to /answerGrid/, will add /playerGrid/ for player input,
    
    displayNumber() {}

--- sudoku/sketch.js
+++ sudoku/sketch.js
@@ -1,28 +1,29 @@
 // Grid Project - Sudoku
 // Avery Walker
-// Oct 29th
+// Oct 30th
 //
 // Extra for Experts:
 //
 
-let grid;
+let answerGrid;
 
 const GRID_SIZE = 9;
 let cellSize;
 //need a variable per cell about whether to show it or not....
-let showAllNumbers = true;
+let showAllNumbers = false;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellSize = 60;
 
-  grid = generateSudoku();
+  answerGrid = generateSudoku();
 }
 
 function draw() {
   background(225);
   displaySudoku();
+  displayNumber();
 }
 
 function displaySudoku() {
@@ -34,27 +35,47 @@ function displaySudoku() {
       textAlign(CENTER, CENTER);
 
       if (showAllNumbers) {
-        text(`${grid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,   y*cellSize + 100 + cellSize/2);
+        text(`${answerGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,   y*cellSize + 100 + cellSize/2);
+      }
+
+    }
+  }
+}
+
+//doesnt work
+// function mouseClicked() {
+//   for (let y = 0; y < GRID_SIZE; y++) {
+//     for (let x = 0; x < GRID_SIZE; x++) {
+//       text(`${answerGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,   y*cellSize + 100 + cellSize/2);
+//     }
+//   }
+// }
+
+function displayNumber() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+
+      if (mouseX > x*cellSize && mouseX < x*cellSize + cellSize && mouseY > y*cellSize && mouseY < y*cellSize + cellSize) {
+
+        text(`${answerGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,
+          y*cellSize + 100 + cellSize/2);
+
       }
     }
   }
 }
 
-function mouseClicked() {
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-    }
-  }
-}
+
+
 
 function generateSudoku() {
-  let possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let possibleNumbers;
   let newGrid = [];
   for (let y = 0; y < GRID_SIZE; y++){
     newGrid.push([]);
     for(let x = 0; x < GRID_SIZE; x++) {
-      //checkViableNumbers(x, y);
-      //push random from whatever numbers ^ returns
+      possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      
       newGrid[y].push(random(possibleNumbers));
     }
   }
@@ -65,9 +86,24 @@ function ChooseHints() {
   //select what cells the player will be able to see
 }
 
-function checkViableNumbers(mainX, mainY) {
-  //needs to check row
-  //check column
-  //check box
-  //return possible numbers
+function checkViableNumbers(currentX, currentY, numberCandidates, theGrid) {
+  let bannedNumbers = [];
+  //use for generation (,,, also use for auto-candidate if i add that?)
+  if (currentX === 0 && currentY === 0) {
+    //first one can be any so dont remove anything
+    return false;
+
+  }
+
+  if (currentY === 0 && currentX !== 0) {
+    //anything in the first row so check all behind
+    for (let x = currentX-1; x < 0; x -= 1) {
+      numberCandidates[0][x] = 0;
+    }
+    //needs to check row
+    //check column
+    //check box
+    // return possible numbers
+  }
+  return numberCandidates;
 }
