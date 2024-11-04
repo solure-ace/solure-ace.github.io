e0a131706032f6b7b3131137d9e7ffad7dc6e913
solure-ace <av.lynne.w@gmail.com>
2024-11-04 14:10:02 -0600

    sudoku - attempt (3x3) generateSudoku()
    reworking displaying the numbers
    tried a bunch of things didnt really improve anything

--- sudoku/sketch.js
+++ sudoku/sketch.js
@@ -3,10 +3,11 @@
 // Oct 30th
 //
 // Extra for Experts:
-//
+//what......
 
 let answerGrid;
 let playerGrid;
+let testGrid = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const GRID_SIZE = 9;
 let cellSize;
@@ -27,12 +28,13 @@ function setup() {
 
 function draw() {
   background(225);
-
-  displaySudoku();
-  displayNumber();
+  if (showAnswerGrid) {
+    displaySudoku(answerGrid);
+  }
+  displaySudoku(playerGrid);
 }
 
-function displaySudoku() {
+function displaySudoku(grid) {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
       fill(30);
@@ -41,21 +43,21 @@ function displaySudoku() {
       textAlign(CENTER, CENTER);
 
       if (showAllNumbers) {
-        text(`${answerGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,   y*cellSize + 100 + cellSize/2);
+        text(`${grid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,   y*cellSize + 100 + cellSize/2);
       }
-
+      displayNumber(grid);
     }
   }
 }
 
-function displayNumber() {
+function displayNumber(theGrid) {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
 
       //            shows number that the mouse hovers over
       if (mouseX > x*cellSize + (width/2-cellSize*9/2) && mouseX <x*cellSize + (width/2-cellSize*9/2) + cellSize 
-      && mouseY > y*cellSize + 100 && mouseY < y*cellSize + 100 + cellSize && showAnswerGrid) {
-        text(`${answerGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,
+      && mouseY > y*cellSize + 100 && mouseY < y*cellSize + 100 + cellSize) {
+        text(`${theGrid[y][x]}`,   x*cellSize + (width/2-cellSize*9/2) + cellSize/2,
           y*cellSize + 100 + cellSize/2);
       }
     }
@@ -68,15 +70,63 @@ function generateSudoku() {
   for (let y = 0; y < GRID_SIZE; y++){
     newGrid.push([]);
     for(let x = 0; x < GRID_SIZE; x++) {
-      // possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-      possibleNumbers = checkViableNumbers(x, y, newGrid);
+      // possibleNumbers = checkViableNumbers(x, y, newGrid);
 
       newGrid[y].push(random(possibleNumbers));
     }
   }
   return newGrid;
 }
+
+// function generateSudoku() {
+//   //trying 3x3 starttt
+//   let newGrid = [];
+//   //0,0 - 2,2
+//   let i = 1;
+//   for (let y = 0; y < GRID_SIZE/3*1; y++){
+//     newGrid.push([]);
+//     for(let x = 0; x < GRID_SIZE/3*1; x++) {
+//       newGrid[y][x] = random([1,2,3,4,5,6,7,8,9]);
+//       if (i<3) {
+//         i++;
+//       }
+//     }
+//   }
+// for (let y = GRID_SIZE/3; y < GRID_SIZE/3*2; y++){
+//   newGrid.push([]);
+//   for(let x = GRID_SIZE/3; x < GRID_SIZE/3*2; x++) {
+//     newGrid[y][x] = random([1,2,3,4,5,6,7,8,9]);
+//   }
+// }
+// for (let y = GRID_SIZE/3*2; y < GRID_SIZE; y++){
+//   newGrid.push([]);
+//   for(let x = GRID_SIZE/3*2; x < GRID_SIZE; x++) {
+//     newGrid[y][x] = random([1,2,3,4,5,6,7,8,9]);
+//   }
+// }
+// }
+
+// function generateSudoku() {
+//   let possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   let newGrid = [];
+//   let y = 0;
+//   for(let x = 0; x < GRID_SIZE; x++) {
+//     theNum = possibleNumbers.pop(random(possibleNumbers.length-1));
+//     console.log(theNum);
+//     // possibleNumbers = checkViableNumbers(x, y, newGrid);
+
+//     newGrid[y].push(random(possibleNumbers));
+//   }
+//   for (let y = 1; y < GRID_SIZE; y++){
+//     newGrid.push([]);
+//     for(let x = 0; x < GRID_SIZE; x++) {
+//       newGrid[y].push(0);
+//     }
+//   }
+//   return newGrid;
+// }
 
 function generateEmptyGrid() {
   let newGrid = [];
