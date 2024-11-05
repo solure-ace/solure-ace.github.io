8143394f38844c4f3e298b61553d381ba2196943
solure-ace <av.lynne.w@gmail.com>
2024-11-05 14:12:24 -0600

    LockSolver- playerInput, EmptyGrid, formatting & platformer demo

--- two-dimensional-grid/sketch.js
+++ two-dimensional-grid/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid = [];
-let playerInput = [1, 3, 3, 4, 5, ];
+let playerInput = [];
 let thePassword = [];
 //x
 let passwordLength = 5;
@@ -38,60 +38,85 @@ function displayGrid() {
   for (let y = 0; y < amountOfGuesses; y++){
     for (let x = 0; x < passwordLength; x++) {
       fill(255);
-      rect(x*cellSize + width/2 - cellSize*passwordLength/2,     y*cellSize + height - cellSize*amountOfGuesses-cellSize*1.5, cellSize, cellSize);
+      // y ?? y*cellSize + height - cellSize*amountOfGuesses-cellSize*1.5
+      rect(x*cellSize + width/2 - cellSize*passwordLength/2, y*cellSize +100, cellSize, cellSize);
+
       textAlign(CENTER, CENTER);
       fill(0);
-      text(`${grid[y][x]}`,x*cellSize + width/2 - cellSize*passwordLength/2 + cellSize/2, y*cellSize + height - cellSize*amountOfGuesses-cellSize*1.5 + cellSize/2);
+      text(`${grid[y][x]}`,x*cellSize + width/2 - cellSize*passwordLength/2 + cellSize/2,
+        y*cellSize +100 + cellSize/2);
     }
   }
 }
 
 function keyPressed() {
+  //keySlot is the 'x'
   let keySlot = 0;
   
-  if (keyCode === BACKSPACE && keySlot !==0) {
+  if (keyCode === BACKSPACE && keySlot !== 0) {
     keySlot -= 1;
   }
+
   // playerInput
   else if (keySlot < amountOfGuesses) {
-    for (let i = 0; i < 9; i++) {
-      if (key === i) {
-        playerInput += `${key}`;
-        keySlot++;
-        return playerInput;
-      }
-    }
+    // for (let i = 0; i < 9; i++) {
+    //   if (key === i) {
+    playerInput += `${key}`;
+    keySlot++;
+    return playerInput;
+    //   }
+    // }
   }
-  else {
-    gameState = "over";
+  else if (keyCode === ENTER && keySlot === passwordLength) {
+    // for each cell replace grid with player input then clear player input
+    //!!!!!!!!!!!!!!! something is breaking here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    playerInput = [];
+    keySlot = 0;
   }
 }
 
 function displayPlayerInput() {
+  //show whatever numbers you have typed untill you hit enter
   fill(255);
-  for (let num = 0; num < passwordLength; num++){
-    text(`${playerInput[num]}`, 30+10*num, 30);  
-    //text(`${thePassword[0]}`30+10*num, 60);
-  }
-  for (let num = 0; num < passwordLength; num++){
-    text(`${thePassword[num]}`, 30+10*num, 60);
 
-  }
+  for (let x = 0; x < passwordLength; x++) {
+    fill(255);
+    rect(x*cellSize + width/2 - cellSize*passwordLength/2, cellSize*amountOfGuesses + cellSize*2.5, cellSize, cellSize);
 
-  function generatePassword() {
-    for( let i = 0; i < passwordLength; i++) {
-      thePassword.push(0);
+    if (playerInput > 0 && x < playerInput.length) {
+      text(`${playerInput[x]}`, 30+10*x, 30); 
+      // text(`${playerInput[x]}`,x*cellSize + width/2 - cellSize*passwordLength/2 + cellSize/2,
+      //   y*cellSize + 100 + cellSize*amountOfGuesses + cellSize*2.5 + cellSize/2);  
     }
   }
 
-  function generateEmptyGrid(){
-    let theGrid = [];
-    for (let y = 0; y < amountOfGuesses; y++) {
-      theGrid.push([]);
-      for (let x = 0; x < passwordLength; x++) {
-        theGrid[y].push(0);
+  if (playerInput > 0) {
+    for (let num = 0; num < passwordLength; num++){
+
+      if (num < playerInput.length){
+        text(`${playerInput[num]}`, 30+10*num, 30);  
       }
     }
-    return theGrid;
+
+    // for (let num = 0; num < passwordLength; num++){
+    //   text(`${thePassword[num]}`, 30+10*num, 60);
+    // }
   }
+}
+
+function generatePassword() {
+  for( let i = 0; i < passwordLength; i++) {
+    thePassword.push(0);
+  }
+}
+
+function generateEmptyGrid(){
+  let theGrid = [];
+  for (let y = 0; y < amountOfGuesses; y++) {
+    theGrid.push([]);
+    for (let x = 0; x < passwordLength; x++) {
+      theGrid[y].push(0);
+    }
+  }
+  return theGrid;
 }
