538cfea07c638d31bb623db8a40c32dbe0fa61c5
solure-ace <av.lynne.w@gmail.com>
2024-11-14 14:15:08 -0600

    lockSolver - lockOpened/win state / yellow tiles

--- two-dimensional-grid/sketch.js
+++ two-dimensional-grid/sketch.js
@@ -1,17 +1,17 @@
 // Lock Solver  /// "wordle but with numbers"
 // Avery Waler 
-// October 11th
+// October 14th
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
 //TO DO
-//generatePassword(){} []
-//yellow/*wrong space* colour coding []
-//win state // game over state []
-//reset game []
-//add some sort of sound when enter is pressed && when win []
+//generatePassword(){} [✔]
+//yellow/*wrong space* colour coding [✔]
+//win state // game over state [✔][ ]
+//reset game [ ]
+//add some sort of sound when enter is pressed && when win [ ]
 
 
 let grid = [];
@@ -24,7 +24,7 @@ let guessCounter = 0;
 //x
 let passwordLength = 5;
 //y
-let amountOfGuesses = 6;
+let amountOfGuesses = 7;
 
 let cellSize = 60;
 let gameState = "start";
@@ -46,7 +46,6 @@ function draw() {
     startScreen();
   }
   if (gameState === "ongoing") {
-    lockOpened = checkifUnlocked();
     background(60);
     displayGrid();
     displayPlayerInputGrid();
@@ -54,7 +53,7 @@ function draw() {
 
     //i want it to keep displaying "ongoing" untill it resets so the lose/winstate is overtop of "ongoing"
     if (lockOpened) {
-
+      text('You Win!', width/2, 50);
     }
   }
 
@@ -70,7 +69,9 @@ function draw() {
 
 
 function startScreen(){
+  textAlign(CENTER, CENTER);
   
+  text('Guess the lock combination to win \n \n GREEN: correct number in the correct spot \n YELLOW: number is in the combination, but in the wrong spot\n \n Click to Start', width/2, height/2-75);
 }
 
 function mousePressed() {
@@ -80,13 +81,12 @@ function mousePressed() {
 }
 
 function checkifUnlocked() {
+
   //check if win
   let slotsUnlocked = 0;
-  for (let y = 0; y < amountOfGuesses; y++){
-    for (let x = 0; x < passwordLength; x++) {
-      if (Number(grid[y][x]) === Number(thePassword[x])) {
-        slotsUnlocked++;
-      }
+  for (let x = 0; x < passwordLength; x++) {
+    if (Number(grid[guessCounter-1][x]) === Number(thePassword[x])) {
+      slotsUnlocked++;
     }
   }
   return slotsUnlocked === passwordLength;
@@ -96,37 +96,37 @@ function checkifUnlocked() {
 
 function keyPressed() {
   //keySlot is the 'x'
-  // can probably replace keyslot by using playerInput.length? maybee... try later
 
-  //backspace
-  if (keyCode === BACKSPACE && keySlot !== 0) { //playerInput.length-1>0
-    keySlot -= 1;
-    playerInput.pop();
-  }
+  if (!lockOpened && gameState === "ongoing") {
+    //backspace
+    if (keyCode === BACKSPACE && keySlot !== 0) { 
+      keySlot -= 1;
+      playerInput.pop();
+    }
 
+    //playerInput // num keys (excluding 0)
+    else if (keySlot < passwordLength && guessCounter < amountOfGuesses) {
+      for (let i = 1; i < 10; i++) {
 
-  //playerInput
-  // !
-  else if (keySlot < passwordLength && guessCounter < amountOfGuesses) { //playerInput.length-1 < amountOfGuesses
-    for (let i = 1; i < 10; i++) {
+        //check if its a number between 1-9
+        if (Number(key) === i) {
+          playerInput.push([`${key}`]);
+          keySlot++;
+        }
 
-      //check if its a number between 1-9
-      if (Number(key) === i) {
-        playerInput.push([`${key}`]);
-        keySlot++;
       }
-
     }
-  }
 
-  // clear the current player input and replace the next open row in the (main) grid
-  else if (keyCode === ENTER && keySlot === passwordLength && guessCounter < amountOfGuesses) { //playerInput.length-1 === passwordLength
-    for (let i = 0; i < passwordLength; i++) {
-      grid[guessCounter][i] = playerInput[i];
+    //clear the current player input and replace the next open row in the (main) grid // enter
+    else if (keyCode === ENTER && keySlot === passwordLength && guessCounter < amountOfGuesses) {
+      for (let i = 0; i < passwordLength; i++) {
+        grid[guessCounter][i] = playerInput[i];
+      }
+      guessCounter++;
+      playerInput = [];
+      keySlot = 0;
+      lockOpened = checkifUnlocked();
     }
-    guessCounter++;
-    playerInput = [];
-    keySlot = 0;
   }
 }
 
@@ -135,12 +135,22 @@ function keyPressed() {
 function displayGrid() {
   for (let y = 0; y < amountOfGuesses; y++){
     for (let x = 0; x < passwordLength; x++) {
+
       fill(255);
+      //numbers that are in the password AND in the right spot are displayed on green tiles
       if (Number(grid[y][x]) === Number(thePassword[x])) {
-        fill("green");
+        fill(20, 150, 60);
       }
 
-      // y ?? y*cellSize + height - cellSize*amountOfGuesses-cellSize*1.5
+      //numbers that are in the password AND NOT in the right spot are displayed on yellow tiles
+      else {
+        for (let i = 0; i < passwordLength; i++) {
+          if (Number(grid[y][x]) === Number(thePassword[i])) {
+            fill(220, 220, 60);
+          }
+        }
+      }
+
       strokeWeight(5);
       stroke(60);
       rect(x*cellSize + width/2 - cellSize*passwordLength/2, y*cellSize +100, cellSize, cellSize);
@@ -148,8 +158,10 @@ function displayGrid() {
       noStroke();
       textAlign(CENTER, CENTER);
       fill(0);
-      text(`${grid[y][x]}`,x*cellSize + width/2 - cellSize*passwordLength/2 + cellSize/2,
-        y*cellSize +100 + cellSize/2);
+      if (grid[y][x] !== 0) {
+        text(`${grid[y][x]}`,x*cellSize + width/2 - cellSize*passwordLength/2 + cellSize/2,
+          y*cellSize +100 + cellSize/2);
+      }
     }
   }
 }
@@ -179,10 +191,10 @@ function displayPlayerInputGrid() {
   //show whatever numbers you have typed untill you hit enter
   fill(255);
   if (guessCounter >= amountOfGuesses && !lockOpened) {
-    fill("red");
+    fill(230, 50, 50);
   }
-  if (lockOpened){
-    fill("green");
+  else if (lockOpened){
+    fill(20, 150, 60);
   }
   for (let x = 0; x < passwordLength; x++) {
     strokeWeight(5);
